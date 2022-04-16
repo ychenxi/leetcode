ba74d9844c2af4ecae4c8facebe0c7f3fd7cea5a
Chenxi Yu <yuchenxi01@gmail.com>
2022-04-15 23:11:25 -0400

    second commit

---
+++ 206.reverse-linked-list.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode second = head.next;
+        if (second == null) {
+            return head;
+        }
+        head.next = null;
+        ListNode result = reverseList(second);
+        second.next = head;
+        return result;
+    }
+}
+// @lc code=end
+
